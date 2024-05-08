55c9084baa5a3b466f479213b82c31bb01cfedb1
gorru135 <gggorru@gmail.com>
2024-05-08 15:00:07 +0900

    240508

--- norazo/src/main/java/edu/kh/norazo/board/model/service/BoardService.java
+++ norazo/src/main/java/edu/kh/norazo/board/model/service/BoardService.java
@@ -5,6 +5,20 @@ import java.util.Map;
 
 public interface BoardService {
 
+	/** 자유 게시판의 지정된 페이지 목록 조회
+	 * @param boardCode
+	 * @param cp
+	 * @return
+	 */
+	Map<String, Object> selectFreeBoardList(String boardCode, int cp);
+
+	/** Faq 게시판의 지정된 페이지 목록 조회
+	 * @param boardCode
+	 * @param cp
+	 * @return
+	 */
+	Map<String, Object> selectFaqBoardList(String boardCode, int cp);
+
 	
 
 }
