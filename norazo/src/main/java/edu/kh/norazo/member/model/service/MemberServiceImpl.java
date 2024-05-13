be74ca0924747335a7109aa1ec071abe1bebb50f
gorru135 <gggorru@gmail.com>
2024-05-13 21:36:32 +0900

    0513

--- norazo/src/main/java/edu/kh/norazo/member/model/service/MemberServiceImpl.java
+++ norazo/src/main/java/edu/kh/norazo/member/model/service/MemberServiceImpl.java
@@ -49,7 +49,7 @@ public class MemberServiceImpl implements MemberService{
 
 	@Override
 	public int checkNickname(String memberNickname) {
-		// TODO Auto-generated method stub
+
 		return mapper.checkNickname(memberNickname);
 	}
 
